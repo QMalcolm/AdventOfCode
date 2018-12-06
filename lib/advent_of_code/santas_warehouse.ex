416a9b677ab157c47c5d2f23796eed8b92f0e9ab
= <tqmalcolm@gmail.com>
2018-12-05 20:37:48 -0800

    Day 2, Part 2

--- lib/advent_of_code/santas_warehouse.ex
+++ lib/advent_of_code/santas_warehouse.ex
@@ -39,4 +39,41 @@ defmodule AdventOfCode.SantasWarehouse do
     end)
     |> is_nil)
   end
+
+  def find_id_intersect() do
+    AdventOfCode.Resource.local_stream("box_ids.txt")
+    |> Enum.reduce([], fn x, acc -> [String.graphemes(x) | acc] end)
+    |> find_id_intersect
+    |> IO.puts
+  end
+
+  defp find_id_intersect([]), do: nil
+  defp find_id_intersect([head | tail]) do
+    case find_diff_of_one(tail, head) do
+      nil ->
+        find_id_intersect(tail)
+      result ->
+        result
+    end
+  end
+
+  defp find_diff_of_one([], _ref), do: nil
+  defp find_diff_of_one([head | tail], ref) do
+    case diff_ids(ref, head) do
+      {1, result} ->
+        result
+      _ ->
+        find_diff_of_one(tail, ref)
+    end
+  end
+
+  defp diff_ids(list1, list2, diff \\ 0, result \\ "")
+  defp diff_ids([], [], diff, result), do: {diff, result}
+  defp diff_ids([head1 | tail1], [head2 | tail2], diff, result) do
+    if head1 == head2 do
+      diff_ids(tail1, tail2, diff, result <> head1)
+    else
+      diff_ids(tail1, tail2, diff + 1, result)
+    end
+  end
 end
